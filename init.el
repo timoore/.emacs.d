515a667401199e03cb60246e0badbbbc7c457e2e
Tim Moore <Tim.Moore@fei.com>
2016-01-15 10:40:59 +0100

    Get org mode from elpa

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" ."http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 ;;; XEmacs
