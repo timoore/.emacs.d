4a8ffafcdadf91eb7344f54ae5e235d365f476d3
Tim Moore <timoore33@gmail.com>
2021-11-18 11:40:42 +0100

    Restore ELPA package repository
    Somehow this get removed when I added MELPA

--- early-init.el
+++ early-init.el
@@ -1,3 +1,4 @@
 (customize-set-variable 'package-archives
 			'(("melpa" ."http://melpa.org/packages/")
+                          ("gnu" . "http://elpa.gnu.org/packages/")
 			  ("org" . "http://orgmode.org/elpa/")))
