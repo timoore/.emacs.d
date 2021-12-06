05151efb94b1d80b5c9f6483c8659a1b769b993d
Tim Moore <timoore33@gmail.com>
2021-12-06 18:22:54 +0100

    lsp stuff

---
+++ lsp.el
@@ -0,0 +1,32 @@
+;;; From https://emacs-lsp.github.io/lsp-mode/tutorials/CPP-guide/
+
+(defvar lsp-packages
+  '(lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company
+             avy which-key helm-xref dap-mode))
+
+(dolist (package lsp-packages)
+  (pushnew package package-selected-packages))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+(which-key-mode)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      ;; clangd is fast
+      lsp-idle-delay 0.1
+      lsp-clients-clangd-args '("-j=4" "-background-index" "-log=error" "--header-insertion-decorators=0"))
+
+(setq lsp-keymap-prefix "s-c")
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
